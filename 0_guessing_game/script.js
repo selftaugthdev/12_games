896b93bbfd51ca7b403ecf3715ea547ad98c37bf
Thierry1980 <thierrydebelder@gmail.com>
2023-11-09 08:25:17 +0100

    Fix logic in computer's guess, finished project

--- 0_guessing_game/script.js
+++ 0_guessing_game/script.js
@@ -1,29 +1,34 @@
 const input = document.getElementById("input");
 const button = document.getElementById("submit");
 const form = document.getElementById("guessForm");
-const output = document.querySelector("output")
+const output = document.querySelector("output");
 let min = 1;
 let max = 25;
 
+function computerGuess(min, max) {
+    // The max - min + 1 ensures we can actually reach the maximum value.
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+} 
+
 form.addEventListener('submit', (event) => {
     event.preventDefault();
-    const userInput = input.value // gets the valu efrom the input field
-    const userGuess = parseInt(userInput, 10) // convert userinput from string to integer
-    const computersGuess = parseInt(computerGuess(), 10)
+    const userInput = input.value;
+    const userGuess = parseInt(userInput, 10);
+    const computersGuess = computerGuess(min, max);
 
-    function computerGuess(min, max) {
-        return Math.floor(Math.random() * (25 - 1));
-    } 
-    // document.write(userGuess, + " " + computerGuess())
+    let resultMessage = ""; // Initialize a variable to hold the result message
 
     if(userGuess < computersGuess) {
-        console.log("You Lost")
+        resultMessage = "You Lost! The computer's guess was higher.";
     } else if (userGuess > computersGuess) {
-        console.log("You Lost")
+        resultMessage = "You Lost! The computer's guess was lower.";
     } else {
-        console.log("You WON!!")
+        resultMessage = "You Won! You guessed the same number as the computer.";
     }
 
-    console.log("My guess is " + userGuess)
-    console.log("Computer's guess is " + computerGuess())
-})
+    // Set the result message to the output element
+    output.textContent = resultMessage;
+
+    // Optional: You might want to log the guesses as well for clarity
+    output.textContent += `\nYour guess: ${userGuess}\nComputer's guess: ${computersGuess}`;
+});
