b3ea481a303f9234748f5f30a484c3fc125eabd3
Thierry De Belder <thierrydebelder@Laptop-van-Thierry.local>
2023-11-10 10:05:03 +0100

    Add function and loop to shuffle the cards

--- 2_21_card_game/script.js
+++ 2_21_card_game/script.js
@@ -4,20 +4,30 @@ const yourSum = 0;
 const dealerAceCount = 0;
 const yourAceCount = 0;
 
-const deck = null;
+const deck = [];
 
 window.onload = function() {
     buildDeck();
+    shuffleDeck();
 }
 
 function buildDeck() {
     const values = ["A", "1", "2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K"];
     const types = ["C", "D", "H", "S"];
-    deck = [];
     for (let i = 0; i < types.length; i++) {
         for (let j = 0; j < values.length; j++) {
             deck.push(values[j] + "-" + types[i]);
         }
     }
-    console.log(deck);
+}
+
+function shuffleDeck() {
+    for (let i = 0; i < deck.length; i++) {
+        const randomIndex = Math.floor(Math.random() * deck.length); //this will giev a number between 0 and 1 * 52 (length of the deck) to give a number between 0 and 52
+        // Swap the card at i with the card at randomIndex
+        const temp = deck[i];
+        deck[i] = deck[randomIndex];
+        deck[randomIndex] = temp;
+    }
+    console.log(deck)
 }
